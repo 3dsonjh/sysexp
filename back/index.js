60ef0ac450e2a9ac49dab3dc1ee6b244bd3c15d3
Edson Alves <alves171@gmail.com>
2023-03-17 21:30:18 -0300

    mongodb202303172

--- back/index.js
+++ back/index.js
@@ -3,7 +3,6 @@ const app = express()
 const port = 3000
 const csv = require('node-csv').createParser();
 
-
 // CRUD
 // C-Create (Criar) R-Read (Ler) U-Update (Atualizar) D-Delete (Apagar)
 
@@ -11,14 +10,11 @@ app.post('/entradas', function(req,res){
    
 });
 
-
 app.get('/entradas', function(req,res){
     csv.parseFile('estoque.csv', function(erro, valores) {
         res.json(valores);
     });
 });
-
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
