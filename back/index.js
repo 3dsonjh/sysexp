5c3121f51d0645eaa06d27bd8ebfcccda93a694d
3dsonjh <alves171@gmail.com>
2023-03-21 21:55:49 -0300

    Conexão com MongoDB

--- back/index.js
+++ back/index.js
@@ -4,6 +4,14 @@ const port = 3000
 const csv = require('node-csv').createParser();
 
 
+//Conectando no MongoDB
+const mongodb = require('mongodb');
+
+const url_mongo = "mongodb+srv://3dsonjh:saidai@cluster0.zdu5eur.mongodb.net/?retryWrites=true&w=majority";
+
+const conexao = new mongodb.MongoClient(url_mongo);
+
+
 // CRUD
 // C-Create (Criar) R-Read (Ler) U-Update (Atualizar) D-Delete (Apagar)
 
@@ -11,11 +19,22 @@ app.post('/entradas', function(req,res){
    
 });
 
-
 app.get('/entradas', function(req,res){
     csv.parseFile('estoque.csv', function(erro, valores) {
         res.json(valores);
     });
+});
+
+// Route / Rota
+app.get('/estoque', async function(req,res){ // Função Assíncrona
+    const estoque = conexao.db("sysexp").collection("estoque");
+    const resultado = await estoque.find({}).toArray(); // await
+    res.json(resultado);
+});
+
+
+app.get('/estoque/:id', function(req,res){
+    res.json(req.params.id)
 });
 
 
